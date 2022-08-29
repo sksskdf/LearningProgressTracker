e3c1e4091ad33562c21924d351d862ae4028ba05
harry <sksskdfg@gmail.com>
2022-08-29 13:12:47 +0900

    test

--- Learning Progress Tracker/task/test/tracker/SampleTest.java
+++ Learning Progress Tracker/task/test/tracker/SampleTest.java
@@ -1,16 +1,19 @@
 package tracker;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class SampleTest {
 
-    @Test
-    public void test() {
+    @ParameterizedTest
+    @CsvSource({"John Doe johnd@email.net, Jane Spark jspark@yahoo.com, 10000 600 400 0 0"})
+    public void test(String inputA, String inputB, String inputC) {
         ClassRoom classRoom = new ClassRoom();
-        classRoom.add("John Doe johnd@email.net");
-        classRoom.add("Jane Spark jspark@yahoo.com");
+        classRoom.add(inputA);
+        classRoom.add(inputB);
         System.out.println(classRoom.students.get(10000));
-        classRoom.addPoints("10000 600 400 0 0");
+        classRoom.addPoints(inputC);
         NotifyService notifyService = new NotifyService(classRoom);
         notifyService.processStudentsToNotify();
         notifyService.processStudentsToNotify();
